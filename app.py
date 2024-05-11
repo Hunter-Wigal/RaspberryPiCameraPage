f9fd18a22d502a7067dac09d8908fee778077f9a
Hunter-Wigal <hgw00002@mix.wvu.edu>
2024-05-11 15:56:50 -0400

    Test change to picamera2

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, Response, url_for, request
-# import the necessary packages
-from picamera.array import PiRGBArray
-from picamera import PiCamera
+import picamera2
 import time
 import cv2
 import os
@@ -9,36 +7,29 @@ import os
 os.environ.setdefault("FLASK_DEBUG", "1")
  
 app = Flask(__name__)
+
+# Set up camera
+picam2 = Picamera2()
+config = picam2.create_video_configuration(...)
+picam2.configure(config)
+picam2.start_preview()
+
  
 @app.route('/')
 def hello_world():
     if request.method == "POST":
         height = request.form['height']
+        
+    if request.method == "GET":
+        stream = io.BytesIO()
+        picam2.capture(stream, 'jpeg', use_video_port=True)
+        stream.seek(0)
+        img = Image.open(stream)
+        return render_template("index.html", img=img)
+        
     return render_template("index.html")
 
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-def gen_frames(): 
-    
-    # initialize the camera and grab a reference to the raw camera capture
-    camera = PiCamera()
-    camera.resolution = (640, 480)
-    camera.framerate = 32
-    rawCapture = PiRGBArray(camera, size=(640, 480))
-    
-    # allow the camera to warmup
-    time.sleep(0.1)
-    # capture frames from the camera
-    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-        # frame = frame.array
-    # while True:
-
-        ret, buffer = cv2.imencode('.jpg', frame)
-        frame = buffer.tobytes()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
 
 @app.route('/test', methods=["POST",  "GET"])
 def test_post():
